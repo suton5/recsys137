30621636bc26046b91134da532a03d56cce1692d
Simon Warchol <me@simonwarchol.com>
2019-12-06 18:54:29 -0500

    Updating w/ new design

--- gulpfile.js
+++ gulpfile.js
@@ -100,7 +100,9 @@ function js() {
   return gulp
     .src([
       './js/*.js',
-      '!./js/*.min.js'
+      '!./js/*.min.js',
+      '!./js/contact_me.js',
+      '!./js/jqBootstrapValidation.js'
     ])
     .pipe(uglify())
     .pipe(header(banner, {
