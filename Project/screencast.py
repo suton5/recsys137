1190a4160e9bc2d9ff862b8a3f6cca3cad5a90e7
Sujay Thakur <suton5@gmail.com>
2019-12-11 18:16:06 -0500

    updated screencast for mood

--- Project/screencast.py
+++ Project/screencast.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-from sklearn.mixture import GaussianMixture
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score,accuracy_score,silhouette_score
 from tqdm import tqdm
+import tensorflow as tf
 import sys
 import spotipy
 import spotipy.util as util
@@ -32,20 +32,35 @@ def SelBest(arr, N):
     idx=np.argsort(arr)[:N]
     return arr[idx]
 
-def recommend_coldstart(song_input, songs_np, songs_df, num_recommend_gmm,
-        num_recommend_nn, gmm_clusters):
+def recommend_mood(mood_input, nn_model, songs_np, songs_df, num_recommend_sample, num_recommend_nn):
     '''Generates song recommendations based on Nearest Neighbours and GMM sampling.
+
     Inputs
-    song_inputs: Index of song that user likes.
+
+    mood_inputs: Mood of the songs that the user wants.
+    nn_model: The neural network model mapping features to mood.
     songs_np: Numpy array of numeric attributes of dataset.
     songs_df: Full dataframe.
+    num_recommend_sample: Number of songs to recommend using sample method.
     num_recommend_nn: Number of songs to recommend using NN.
-    num_recommend_gmm: Number of songs to recommend using GMM sampling.
-    gmm_clusters: Number of clusters for GMM model. Will find optimal if specified as 0.
+
     Outputs
-    nn_recc_songs: Recommendations using NN.
-    gmm_recc_songs: Recommendations using GMM.
+    
+    sample_recc_songs: Recommendations using sample method.
+    nn_recc_songs: Recommendations using neural network method.
     '''
+    
+    #Sample method
+    
+    sample_playlist = pd.read_csv('../test_data/'+mood_input+'.csv')
+    sample_song_size = 0
+    while sample_song_size == 0:
+        #keep resampling till we get a song in our playlist
+        sample_idx = np.random.randint(low=0, high=sample_playlist.shape[0], size=1)[0]
+        sample_trackuri = sample_playlist.iloc[sample_idx]['track_uri']
+        sample_song_idx = songs_df[songs_df['track_uri'] == sample_trackuri].index
+        sample_song_size = sample_song_idx.size
+    song_input = sample_song_idx[0]
 
     query_song = songs_np[song_input]
     playlist_idx = songs_df[songs_df['track_uri'] == songs_df.iloc[song_input]['track_uri']]['pid'].values
@@ -53,71 +68,47 @@ def recommend_coldstart(song_input, songs_np, songs_df, num_recommend_gmm,
     idx = query_songs_df.drop_duplicates(subset=['track_uri']).index.values
     query_songs_np = songs_np[idx]
 
-    if gmm_clusters==0:
-        #Do tuning
-        print("Tuning hyperparameters for GMM.")
-
-        n_clusters=np.arange(2, 10)
-        sils=[]
-        bics=[]
-        iterations=20
-        for n in tqdm(n_clusters):
-            tmp_sil=[]
-            tmp_bic=[]
-            for _ in range(iterations):
-                gmm=GaussianMixture(n, n_init=2).fit(query_songs_np)
-                labels=gmm.predict(query_songs_np)
-                sil=silhouette_score(query_songs_np, labels, metric='euclidean')
-                tmp_sil.append(sil)
-                tmp_bic.append(gmm.bic(query_songs_np))
-            val=np.mean(SelBest(np.array(tmp_sil), int(iterations/5)))
-            sils.append(val)
-            val=np.mean(SelBest(np.array(tmp_bic), int(iterations/5)))
-            bics.append(val)
-        gmm_clusters = int((n_clusters[np.argmin(bics)] + n_clusters[np.argmax(sils)])/2)
-
-        print("Optimal number of clusters: {}.".format(gmm_clusters))
-
-    print("Fitting models.")
-
-    gmm = GaussianMixture(n_components=gmm_clusters).fit(query_songs_np)
     nn = NearestNeighbors().fit(query_songs_np)
-
-    print("Generating recommendations.")
-
-    #GMM sampling
-    label_gmm = gmm.predict(query_song.reshape(1,-1))[0]
-    #to ensure we get 10 recommendations
-    num_being_recommended = 0
-    while num_being_recommended < num_recommend_gmm:
-        samples = np.random.multivariate_normal(gmm.means_[label_gmm], gmm.covariances_[label_gmm], 2*num_recommend_gmm)
-        dist, indices = nn.kneighbors(samples, n_neighbors=1)
-        #drop possible duplicates
-        gmm_recc = list(set(indices.flatten()))[:num_recommend_gmm]
-        num_being_recommended = len(gmm_recc)
-
-    #NN
-    dist, indices = nn.kneighbors(query_song.reshape(1,-1), n_neighbors=num_recommend_nn+1)
+    dist, indices = nn.kneighbors(query_song.reshape(1,-1), n_neighbors=num_recommend_sample+1)
     nn_recc = indices.flatten()[1:]
 
-    nn_recc_songs = songs_df.iloc[idx].iloc[nn_recc]
-    gmm_recc_songs = songs_df.iloc[idx].iloc[gmm_recc]
+    sample_recc_songs = songs_df.iloc[idx].iloc[nn_recc]
     
-    return nn_recc_songs, gmm_recc_songs
+    #Neural Network
 
-if sys.argv[2] == 'Versace':
-	inpt = 382
+    idx = songs_df.drop_duplicates(subset=['track_uri']).index.values
+    nodup_songs_np = songs_np[idx]
+    y_pred = nn_model.predict_classes(nodup_songs_np)
+    if mood_input == 'angry':
+        check = 0, 
+    elif mood_input == 'party':
+        check = 1, 
+    elif mood_input == 'sad':
+        check = 2
+    else:
+        check = 3
+
+    rec_idx_all = np.argwhere(y_pred==check)
+    np.random.shuffle(rec_idx_all)
+    rec_idx = rec_idx_all[:num_recommend_nn]
+    
+    nn_recc_songs = songs_df.iloc[idx].iloc[rec_idx.flatten()]
+    
+    return sample_recc_songs, nn_recc_songs
+
+model = tf.keras.models.load_model('model.h5')
+print("Loaded neural network model from disk")
 
 df, X_unscaled, X_train = preprocess_numeric(str(sys.argv[1]))
-nn_recc_songs, gmm_recc_songs = recommend_coldstart(inpt, X_train, df, 10, 10, int(sys.argv[3]))
+sample_recc_songs, nn_recc_songs = recommend_mood(str(sys.argv[2]), model, X_train, df, 10, 10)
+
+# Sample
+print('\nSample-based Recommendations\n')
+print(sample_recc_songs[['track_name', 'artist_name']])
 
 # NN
-print('\nNN Recommendations\n')
+print('\nNN-based Recommendations\n')
 print(nn_recc_songs[['track_name', 'artist_name']])
-
-# NN
-print('\nGMM Recommendations\n')
-print(gmm_recc_songs[['track_name', 'artist_name']])
 
 
 # As before, the code is commented out to avoid an error
@@ -132,17 +123,17 @@ except (AttributeError, JSONDecodeError):
 spotify = spotipy.Spotify(auth=token)
 
 
-new_playlist = spotify.user_playlist_create(username, 'NN Recommendations', public=True)
+new_playlist = spotify.user_playlist_create(username, 'Sample-based Recommendations', public=True)
 
-for i in range(nn_recc_songs.shape[0]):
-	id_x = nn_recc_songs['track_uri'].values[i]
+for i in range(sample_recc_songs.shape[0]):
+	id_x = sample_recc_songs['track_uri'].values[i]
 	id_x = [id_x]
 	results = spotify.user_playlist_add_tracks(username, new_playlist['id'], id_x)
 
-new_playlist = spotify.user_playlist_create(username, 'GMM Recommendations', public=True)
+new_playlist = spotify.user_playlist_create(username, 'NN-based Recommendations', public=True)
 
-for i in range(gmm_recc_songs.shape[0]):
-	id_x = gmm_recc_songs['track_uri'].values[i]
+for i in range(nn_recc_songs.shape[0]):
+	id_x = nn_recc_songs['track_uri'].values[i]
 	id_x = [id_x]
 	results = spotify.user_playlist_add_tracks(username, new_playlist['id'], id_x)
 
